5286bf1718e46319d39e447980ef43619e3f7a38
cheeming <dragon.steal75@gmail.com>
2024-03-14 00:01:16 +0900

    fix: add cryptotype filter

--- src/shared/services/http/get-crypto-currencies.ts
+++ src/shared/services/http/get-crypto-currencies.ts
@@ -8,11 +8,11 @@ export const getCryptoCurrenciesList = async (apiUrl: string) => {
       .get(`${apiUrl}/v3/cryptocurrency/listing`, {
         params: {
           start: 1,
-          limit: 100,
+          limit: 10,
           sortBy: 'market_cap', // market_cap, circulating_supply, price, volume_24h
           sortType: 'desc',
           convert: 'USD,BTC,ETH',
-          //   cryptoType: 'all',
+          cryptoType: 'all',
           //   tagType: 'all',
           //   audited: false,
           //   'ath,atl,high24h,low24h,num_market_pairs,cmc_rank,date_added,tags,platform,max_supply,circulating_supply,self_reported_circulating_supply,self_reported_market_cap,total_supply,volume_7d,volume_30d',
